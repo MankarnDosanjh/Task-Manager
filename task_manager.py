fa849a3048ff195ded8adb2931405174666c85fa
MankarnDosanjh <MankarnDosanjh114@gmail.com>
2023-12-30 13:16:51 +0000

    reg_user function no longer allows duplicate usernames

--- task_manager.py
+++ task_manager.py
@@ -42,29 +42,34 @@ for t_str in task_data:
 def reg_user():
     '''Add a new user to the user.txt file'''
     # - Request input of a new username
-    new_username = input("New Username: ")
+    while True:
+        new_username = input("New Username: ")
+        if new_username in username_password.keys():
+            print("Username already exists please try again")
+            continue
+        # - Request input of a new password
+        new_password = input("New Password: ")
 
-    # - Request input of a new password
-    new_password = input("New Password: ")
+        # - Request input of password confirmation.
+        confirm_password = input("Confirm Password: ")
 
-    # - Request input of password confirmation.
-    confirm_password = input("Confirm Password: ")
+        # - Check if the new password and confirmed password are the same.
+        if new_password == confirm_password:
+            # - If they are the same, add them to the user.txt file,
+            print("New user added")
+            username_password[new_username] = new_password
+            
+            with open("user.txt", "w") as out_file:
+                user_data = []
+                for k in username_password:
+                    user_data.append(f"{k};{username_password[k]}")
+                out_file.write("\n".join(user_data))
+            break
 
-    # - Check if the new password and confirmed password are the same.
-    if new_password == confirm_password:
-        # - If they are the same, add them to the user.txt file,
-        print("New user added")
-        username_password[new_username] = new_password
-        
-        with open("user.txt", "w") as out_file:
-            user_data = []
-            for k in username_password:
-                user_data.append(f"{k};{username_password[k]}")
-            out_file.write("\n".join(user_data))
-
-    # - Otherwise you present a relevant message.
-    else:
-        print("Passwords do no match")
+        # - Otherwise you present a relevant message.
+        else:
+            print("Passwords do not match")
+            continue
 
 
 def add_task():
